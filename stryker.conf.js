09c77a197803dd52b7e14bd2bce035905b6a7a00
Valerie Graybeal <valerie.graybeal@globant.com>
2019-02-06 13:48:26 -0500

    Hero App v1

---
+++ stryker.conf.js
@@ -0,0 +1,24 @@
+// This config was generated using a preset.
+// Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/angular.md#angular
+module.exports = function(config) {
+  config.set({
+    mutate: [
+      "src/**/*.ts",
+      "!src/**/*.spec.ts",
+      "!src/test.ts",
+      "!src/environments/*.ts"
+    ],
+    mutator: "typescript",
+    testRunner: "karma",
+    karma: {
+      configFile: "src/karma.conf.js",
+      projectType: "angular-cli",
+      config: {
+        browsers: ["ChromeHeadless"]
+      }
+    },
+    reporters: ["progress", "clear-text", "html"],
+    maxConcurrentTestRunners: 6, // Recommended to use about half of your available cores when running stryker with angular.
+    coverageAnalysis: "off"
+  });
+};

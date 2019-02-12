dda1e4cfc31ceb04f87df23900ff1d1d2c056b87
Valerie Graybeal <valerie.graybeal@globant.com>
2019-02-12 11:01:33 -0500

    Got heroes component to work with jest

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,22 +1,49 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { HeroesComponent } from './heroes.component';
+import { Store } from '@ngrx/store';
+import { Subject } from 'rxjs';
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let fixture: ComponentFixture<HeroesComponent>;
+  let unsubscribe = new Subject<void>();
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroesComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      declarations: [ HeroesComponent ],
+      providers: [
+        {
+          provide: Store,
+          useValue: { 
+            // How we did it before
+            select: jest.fn(),
+            pipe: jest.fn(),
+            dispatch: jest.fn()
+          },
+        }
+      ]
     })
     .compileComponents();
   }));
+  
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeroesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    unsubscribe.next();
+    unsubscribe.complete();
   });
 
   it('should create', () => {

722ed1b3bcc8fc73107b968264d4b9183202e646
Valerie Graybeal <valerie.graybeal@globant.com>
2019-02-12 15:50:46 -0500

    Got hero detail tests working

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Location } from '@angular/common';
+
+export class MockActivatedRoute {
+  constructor(public params: MockParams) {}
+}
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
@@ -8,7 +16,27 @@ describe('HeroDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroDetailComponent ]
+      imports: [
+        FormsModule, 
+        HttpClientTestingModule
+      ],
+      declarations: [ HeroDetailComponent ],
+      providers: [{
+          provide: ActivatedRoute, 
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: jest.fn()
+              }
+            }
+          },
+        },{
+          provide: Location, 
+          useValue: {
+            back: jest.fn()
+          },
+        },
+      ]
     })
     .compileComponents();
   }));

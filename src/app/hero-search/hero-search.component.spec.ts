0cf8035e84ea10cd3217f8608c62ee071035f06b
Valerie Graybeal <valerie.graybeal@globant.com>
2019-02-27 16:01:15 -0500

    Updating hero search spec

--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -1,8 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { HeroSearchComponent } from './hero-search.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { HeroSearchComponent } from './hero-search.component';
+
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+import { of, Observable } from 'rxjs';
+
+
+class MockHeroService {
+  searchHeroes(id: number): Observable<Hero[]> {
+    return of([{}] as Hero[]);
+  }
+}
 
 describe('HeroSearchComponent', () => {
   let component: HeroSearchComponent;
@@ -15,6 +26,11 @@ describe('HeroSearchComponent', () => {
         RouterTestingModule,
         HttpClientTestingModule
       ],
+      providers: [{
+        provide: HeroService,
+        useClass: MockHeroService
+      }
+    ]
     })
     .compileComponents();
   }));
@@ -25,7 +41,38 @@ describe('HeroSearchComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+
+  describe('search', () => {
+    let heroService;
+    beforeEach(() => {
+      jasmine.clock().install();
+      heroService = TestBed.get(HeroService);
+      spyOn(heroService, 'searchHeroes').and.callThrough();
+    })
+    afterEach(function() {
+      jasmine.clock().uninstall();
+    });
+    describe('searching twice with same term, then once with new term', () => {
+      it('should search only once with same search term, then search with new term', () => {
+        // Search once
+        component.search('who');
+        // Fast forward time for debounce
+        jasmine.clock().tick(1000);
+        expect(heroService.searchHeroes).toHaveBeenCalledTimes(1);
+        expect(heroService.searchHeroes).toHaveBeenCalledWith('who');
+        // Search again with the same term
+        component.search('who');
+        // Fast forward time
+        jasmine.clock().tick(1000);
+        expect(heroService.searchHeroes).toHaveBeenCalledTimes(1);
+     
+        // Search again with the new term
+        component.search('what');
+        // Fast forward time
+        jasmine.clock().tick(1000);
+        expect(heroService.searchHeroes).toHaveBeenCalledTimes(2);
+        expect(heroService.searchHeroes).toHaveBeenCalledWith('what');
+      })
+    })
   });
 });

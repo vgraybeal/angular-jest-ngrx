11bc29da78e6152a92b4843792117d8b4503ec05
ryshep111 <ryshep111@gmail.com>
2019-03-05 10:37:08 -0500

    ngrx store freeze

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
+import { MetaReducer, StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { heroesReducer } from './store/hero.reducer';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
-import { initialState } from './store/state';
+import { AppState, initialState } from './store/state';
+import { storeFreeze } from 'ngrx-store-freeze';
+import { environment } from '../environments/environment';
+
+const metaReducers: MetaReducer<AppState>[] = !environment.production ? [storeFreeze]: []
 
 export const baseImports = [
   BrowserModule,
@@ -20,7 +24,7 @@ export const imports = [
   HttpClientModule,
   StoreModule.forRoot({
     heroes: heroesReducer
-  }, {initialState}),
+  }, {initialState, metaReducers}),
   StoreDevtoolsModule.instrument({maxAge: 25}), // Must be imported AFTER StoreModule
   HttpClientInMemoryWebApiModule.forRoot(
     InMemoryDataService, { dataEncapsulation: false }
